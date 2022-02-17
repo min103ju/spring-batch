fd09960e5ad6325ddde14e02a596975b8f5e0a05
Min Heo <min103ju@gmail.com>
2022-02-17 23:43:36 +0900

    queryDsl 의존성 추가

--- src/main/java/com/citizen/springbatch/jobs/BatchConfig.java
+++ src/main/java/com/citizen/springbatch/jobs/BatchConfig.java
@@ -1,15 +1,19 @@
 package com.citizen.springbatch.jobs;
 
-import com.citizen.springbatch.tasklets.BatchTasklet;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// @Configuration과 @Bean을 통해 Spring Container에 등록을 해야 Job과 Step이 구동한다.
+// @Configuration과 @Bean을 통해 Spring Container에 Job과 Step을 등록해야한다.
+@Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class BatchConfig {
@@ -25,17 +29,31 @@ public class BatchConfig {
     @Bean
     public Job batchJob() {
         return jobBuilderFactory.get(JOB_NAME)
-            .start(batchStep())
+            .start(batchStep(null))
             .build();
     }
 
     // StepBuilderFactory를 통해 Step 생성
     // public으로 생성
     @Bean
-    public Step batchStep() {
+    @JobScope
+    public Step batchStep(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get(STEP_NAME)
-            .tasklet(new BatchTasklet())
+            // Tasklet은 Step 안에서 단일로 수행될 커스텀한 기능들을 선언할 때 사용
+            .tasklet((contribution, chunkContext) -> {
+                log.info(">>>>requestDate : {}", requestDate);
+                return RepeatStatus.FINISHED;
+            })
             .build();
     }
+
+    /**
+     * Job > Step > Tasklet 또는 Reader, Processor, Writer
+     * Job안에 여러개의 Step이 존재
+     * Step안에 단일로 수행될 Tasklet이 존재
+     * 단, Tasklet은 Reader & Processor & Writer 한 묶음과 같은 레벨이다.
+     * 따라서 Reader & Processor & Writer 진행 후 Tasklet으로 마무리하는 등의 작업은 만들 수 없다.
+     * (Tasklet은 @Component와 비슷하다. 명확한 역할은 없지만, 개발자가 지정한 커스텀 기능을 위한 단위이다.)
+     */
 
 }
--- src/main/java/com/citizen/springbatch/tasklets/BatchTasklet.java
+++ src/main/java/com/citizen/springbatch/tasklets/BatchTasklet.java
@@ -1,23 +1,29 @@
 package com.citizen.springbatch.tasklets;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 
 // Job -> Step에서 실행할 Tasklet을 만든다.
 // org.springframework.batch.core.step.tasklet의 Tasklet을 구현한다.
 @Slf4j
+@RequiredArgsConstructor
 public class BatchTasklet implements Tasklet {
 
     private static int TASKLET_EXECUTE_COUNT = 0;
 
+    private final String requestDate;
+
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
         throws Exception {
-
-            log.info("execute tasklet!! TASKLET_EXECUTE_COUNT : {}", ++TASKLET_EXECUTE_COUNT);
+        log.info("execute tasklet!! TASKLET_EXECUTE_COUNT : {}", ++TASKLET_EXECUTE_COUNT);
+        log.info("requestDate: {}", requestDate);
         return RepeatStatus.FINISHED;
     }
 }
