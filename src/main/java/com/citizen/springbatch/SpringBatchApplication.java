8ee0ff702433977b4342f267a82bc96e161655d7
Min Heo <min103ju@gmail.com>
2022-02-20 17:38:59 +0900

    스케쥴러가 아닌 application 구동으로만 돌아가게끔 수정

--- src/main/java/com/citizen/springbatch/SpringBatchApplication.java
+++ src/main/java/com/citizen/springbatch/SpringBatchApplication.java
@@ -3,15 +3,13 @@ package com.citizen.springbatch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling // 스케쥴러 기능 활성화
 @EnableBatchProcessing // Batch 기능 활성화
 @SpringBootApplication
 public class SpringBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
 
 }
