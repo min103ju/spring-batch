bc04db07a69c6cd23bcc983a70e48266457ace63
Min Heo <min103ju@gmail.com>
2022-02-27 02:03:17 +0900

    JdbcCursorItemReader 설명 주석 추가

--- src/main/java/com/citizen/springbatch/jobs/JdbcCursorItemReaderJobConfiguration.java
+++ src/main/java/com/citizen/springbatch/jobs/JdbcCursorItemReaderJobConfiguration.java
@@ -44,6 +44,8 @@ public class JdbcCursorItemReaderJobConfiguration {
     @Bean
     private Step jdbcCursorItemReaderStep() {
         return stepBuilderFactory.get(STEP_NAME)
+            // 첫 번째는 Reader에서 반환할 타입, 두 번째는 Writer에 인자로 넘어올 타입
+            // chunk() 메소드에서 size를 지정할 경우 reader & writer가 묶일 트랜잭션 범위이다.
             .<Post, Post>chunk(CHUNK_SIZE)
             .reader(jdbcCursorItemReader())
             .writer(jdbcCursorItemWriter())
@@ -55,10 +57,15 @@ public class JdbcCursorItemReaderJobConfiguration {
         String sql = "SELECT id, title, content From Post";
 
         return new JdbcCursorItemReaderBuilder<Post>()
-            .fetchSize(CHUNK_SIZE)
+            .fetchSize(CHUNK_SIZE) // reader에서 조회할 데이터 양
             .dataSource(dataSource)
+            // 쿼리 결과를 Java 인스턴스로 매핑하기 위한 Mapper
+            // 커스텀하게 생성해서 할 수도 있지만, 매번 Mapper 클래스를 사용하는 것보다
+            // 보편적으로 사용하는 BeanPropertyRowMapper를 사용하자
             .rowMapper(new BeanPropertyRowMapper<>(Post.class))
             .sql(sql)
+            // reader의 이름을 지정
+            // Bean이 아니며, Spring Batch의 ExecutionContext에 저장될 이름
             .name(READER_NAME)
             .build();
     }
