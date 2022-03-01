b20852becd07124aee98a1a286c75df4e26aad7a
Min Heo <min103ju@gmail.com>
2022-03-02 03:58:19 +0900

    Writer에 List 타입 전달하는 ItemListJobConfiguration 추가

---
+++ src/main/java/com/citizen/springbatch/jobs/ItemListJobConfiguration.java
@@ -0,0 +1,67 @@
+package com.citizen.springbatch.jobs;
+
+import com.citizen.springbatch.domain.Post;
+import com.citizen.springbatch.domain.Post2;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * author : citizen103
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class ItemListJobConfiguration {
+
+    private static final int CHUNK_SIZE = 10;
+
+    private static final String JOB_NAME = "itemListJob";
+    private static final String STEP_NAME = "itemListStep";
+    private static final String READER_NAME = "itemListReader";
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public Job itemListJob() {
+        return jobBuilderFactory.get(JOB_NAME)
+            .start(itemListStep())
+            .build();
+    }
+
+    @Bean
+    public Step itemListStep() {
+        return stepBuilderFactory.get(STEP_NAME)
+            .<Post, List<Post2>>chunk(CHUNK_SIZE)
+            .reader(itemListReader())
+            .writer(itemListWriter())
+            .build();
+    }
+
+    public JpaPagingItemReader<Post> itemListReader() {
+        JpaPagingItemReader<Post> reader = new JpaPagingItemReader<>();
+        reader.setPageSize(CHUNK_SIZE);
+        reader.setQueryString("SELECT p From Post p");
+        reader.setEntityManagerFactory(entityManagerFactory);
+        return reader;
+    }
+
+    public ItemWriter<List<Post2>> itemListWriter() {
+        JpaItemWriter<List<Post2>> writer = new JpaItemWriter<>();
+        writer.setEntityManagerFactory(entityManagerFactory);
+        return writer;
+    }
+
+}
